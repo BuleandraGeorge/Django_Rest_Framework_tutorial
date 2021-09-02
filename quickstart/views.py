4eb6e132a73e075c950b2d40803d24427522593d
BuleandraGeorge <buleandrageorge@gmail.com>
2021-09-02 15:25:29 +0000

    End of Quickstart

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from .serialisers import UserSerializer, GroupSerializer
+from rest_framework import viewsets, permissions
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset=Group.objects.all()
+    serializer_class=GroupSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
 
 # Create your views here.
